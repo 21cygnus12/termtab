55e65a7594d8b258c67357ccd9b2dcf09b4d66d4
21cygnus12 <zackgwaterman@gmail.com>
2025-09-10 04:33:26 -0700

    started modularizing

--- src/main.rs
+++ src/main.rs
@@ -1,83 +1,8 @@
-use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
-use ratatui::{DefaultTerminal, Frame, widgets::Paragraph};
-
-enum Message {
-    ChangeMode(Mode),
-    Quit,
-}
-
-#[derive(Default, Debug)]
-enum Mode {
-    #[default]
-    Normal,
-    Insert,
-}
-
-#[derive(Default)]
-struct App {
-    mode: Mode,
-    status: AppStatus,
-}
-
-#[derive(Default)]
-enum AppStatus {
-    #[default]
-    Running,
-    Done,
-}
-
-impl App {
-    fn new() -> Self {
-        App::default()
-    }
-
-    fn view(&self, frame: &mut Frame) {
-        frame.render_widget(
-            Paragraph::new(format!("Mode: {:?}", self.mode)),
-            frame.area(),
-        );
-    }
-
-    fn update(&mut self, message: Message) {
-        match message {
-            Message::ChangeMode(mode) => self.mode = mode,
-            Message::Quit => self.status = AppStatus::Done,
-        }
-    }
-
-    fn handle_key_press(key_event: KeyEvent) -> Option<Message> {
-        match key_event.code {
-            KeyCode::Char('q') => Some(Message::Quit),
-            KeyCode::Esc => Some(Message::ChangeMode(Mode::Normal)),
-            KeyCode::Char('i') => Some(Message::ChangeMode(Mode::Insert)),
-            _ => None,
-        }
-    }
-
-    fn handle_event() -> std::io::Result<Option<Message>> {
-        match event::read()? {
-            Event::Key(key) if key.kind == KeyEventKind::Press => {
-                Ok(Self::handle_key_press(key))
-            }
-            _ => Ok(None),
-        }
-    }
-
-    fn run(&mut self, terminal: &mut DefaultTerminal) -> std::io::Result<()> {
-        while let AppStatus::Running = self.status {
-            terminal.draw(|frame| self.view(frame))?;
-            let current_message = Self::handle_event()?;
-            if current_message.is_some() {
-                self.update(current_message.unwrap());
-            }
-        }
-        Ok(())
-    }
-}
+use termtab::app;
 
 fn main() -> std::io::Result<()> {
     let mut terminal = ratatui::init();
-    let mut app = App::new();
+    let mut app = app::App::new();
     let result = app.run(&mut terminal);
     ratatui::restore();
     result
